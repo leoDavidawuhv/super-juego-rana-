3850b97a3d2ddac38a97d24d975fbc0efd44aac3
leoDavidawuhv <93741517+leoDavidawuhv@users.noreply.github.com>
2022-04-06 12:49:43 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# super-juego-rana-
+# C42_Código de referencia_Carreras de autos
+Código de referencia
---
+++ sketch.js
@@ -0,0 +1,12 @@
+var alienrana,alienranaimg;
+var bola,bolaimg;
+var ateroides,ateroidesimg;
+var background,backgroundimg;
+var planeta,planetaimg;
+function setup(){
+createCanvas(600,600);
+alienrana=createSprite(300,100,20,20);
+bola=createSprite(300,100,10,10);
+planeta=createSprite(300,580,20,20);
+
+}
